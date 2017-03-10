a166df5fa3fa4a1c4026a0a78b2651506f81248b
Jenny Nguyen <jenny.nguyen@miiniejourney.com>
2017-03-10 01:46:12 -0800

    init commit
    load website into electron app view

---
+++ src/main.js
@@ -0,0 +1,42 @@
+'use strict';
+process.env.ELECTRON_HIDE_INTERNAL_MODULES = 'true';
+
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('window-all-close', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+var startupOpts = {
+    useContentSize: true,
+    width: 800,
+    height: 600,
+    center: true,
+    resizable: true,
+    alwaysOnTop: false,
+    fullscreen: false,
+    skipTaskbar: true,
+    kiosk: false,
+    title: '',
+    icon: null,
+    show: false,
+    frame: true,
+    disableAutoHideCursor: false,
+    autoHideMenuBar: false,
+    titleBarStyle: 'default'
+};
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow(startupOpts);
+  mainWindow.loadUrl('https://miiGit.github.io');
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+  mainWindow.show();
+});
